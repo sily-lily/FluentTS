2267466a8f8e804ad70f0848de848e6f181e7762
Suno <ilovesuno@proton.me>
2024-10-19 18:45:33 -0700

    Change optional stuff

--- types/slider.d.ts
+++ types/slider.d.ts
@@ -8,12 +8,12 @@
 
 export interface SliderOptions {
     Title: string;                          // The title (header) of the slider.
-    Description?: string;                   // The description of the slider, what it's about.
-    Default: number;                        // The default (starting) value for the slider. (min through max)
+    Description?: string;                   // The description of the slider, what it's about. (optional)
+    Default?: number;                        // The default (starting) value for the slider. (min through max) (optional)
     Min: number;                            // The minimum value for the slider.
     Max: number;                            //     maximum
-    Rounding?: number;                      // Where the decimal is placed. (Lua index starts at 1)
-    Callback?: (value: number) => void;     // The function that is called when the slider changes.
+    Rounding?: number;                      // Where the decimal is placed. (Lua index starts at 1) (optional)
+    Callback: (value: number) => void;      // The function that is called when the slider changes.
 }
 
 export interface Slider {
