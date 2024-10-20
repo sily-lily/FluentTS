766088e1f16d000a24c1897e5cf8c4362f61d77a
Suno <ilovesuno@proton.me>
2024-10-19 18:46:04 -0700

    Change optional stuff

--- types/keybind.d.ts
+++ types/keybind.d.ts
@@ -10,11 +10,11 @@
 */
 
 export interface KeybindOptions {
-    Title: string;                                  // The title (header) of the keybind.
-    Mode: "Always" | "Toggle" | "Hold";             // The mode used for the keybind. (no explanation on what they do)
-    Default: string;                                // The default key for the keybind. (not an Enum, auto converts)
-    Callback: (value: boolean) => void;             // Function called when the keybind is pressed.
-    ChangedCallback: (newKey: string) => void;      // The function called when the keybind changes.
+    Title: string;                                   // The title (header) of the keybind.
+    Mode: "Always" | "Toggle" | "Hold";              // The mode used for the keybind. (no explanation on what they do)
+    Default?: string;                                // The default key for the keybind. (not an Enum, auto converts)
+    Callback: (value: boolean) => void;              // Function called when the keybind is pressed.
+    ChangedCallback: (newKey: string) => void;       // The function called when the keybind changes.
 }
 
 export interface Keybind {
