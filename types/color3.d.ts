36bbc9ac13478985bbb591b398eead9a7e59f475
Suno <ilovesuno@proton.me>
2024-11-25 16:15:08 -0700

    Update color3.d.ts

--- types/color3.d.ts
+++ types/color3.d.ts
@@ -1,15 +1,17 @@
-export class _Color3 {
-    r: number;
-    g: number;
-    b: number;
+declare module "_Color3" {
+    export class Color3 {
+        r: number;
+        g: number;
+        b: number;
 
-    constructor(r: number, g: number, b: number);
+        constructor(r: number, g: number, b: number);
 
-    /**
-     * Create a `_Color3` object from RGB values. (red, green, blue)
-     * @param r - Red (0 - 255)
-     * @param g - Green
-     * @param b - Blue
-     */
-    static fromRGB(r: number, g: number, b: number): Color3;
+        /**
+         * Create a `Color3` object from RGB values. (red, green, blue)
+         * @param r - Red (0 - 255)
+         * @param g - Green (0 - 255)
+         * @param b - Blue (0 - 255)
+         */
+        static fromRGB(r: number, g: number, b: number): Color3;
+    }
 }
