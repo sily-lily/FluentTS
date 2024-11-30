9fa229fefc46fadd27c22ae46edf16956703ce7c
Suno <ilovesuno@proton.me>
2024-11-30 03:33:26 -0700

    Update colorpicker.d.ts

--- types/colorpicker.d.ts
+++ types/colorpicker.d.ts
@@ -8,12 +8,14 @@
               .SetValueRGB
               .OnChanged
               .Destroy
+              .Value
 
 */
 
 export interface ColorPickerOptions {
-    Title: string;     // The title of the color picker.
-    Default?: any;  // The default value of the color picker. (optional)
+    Title: string;                      // The title of the color picker.
+    Default?: Color3;                   // The default value of the color picker. (optional)
+    Callback?: (color: Color3) => void; // Does an action when a color is changed
 }
 
 export interface ColorPicker {
@@ -39,10 +41,24 @@ export interface ColorPicker {
     SetValue(h: number, s: number, v: number): void;
 
     /**
-    * Function that's called when the color value changes.
-    * @param callback - Function that intakes the new HSV values.
-    */
+     * Set the value of the color picker in RGB format.
+     * @param r - Red value (0 - 255)
+     * @param g - Green value (0 - 255)
+     * @param b - Blue value (0 - 255)
+     */
+    SetValueRGB(r: number, g: number, b: number): void;
+
+    /**
+     * Function that's called when the color value changes.
+     * @param callback - Function that intakes the new HSV values.
+     */
     OnChanged(callback: (color: Color3) => void): void;
+
+    /**
+     * Get the currently selected value of the color picker.
+     * @returns - The current color value as a `Color3`.
+     */
+    get Value(): Color3;
 
     /**
      * Destroy the color picker object.
