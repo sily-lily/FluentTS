dedd5ccc68f2830aa3168e6ed5c501e92215aede
Suno <ilovesuno@proton.me>
2025-02-19 02:24:20 -0700

    Update button.d.ts

--- types/button.d.ts
+++ types/button.d.ts
@@ -1,5 +1,6 @@
 export interface ButtonOptions {
     Title: string;          // The title (header) of the button object.
     Description?: string;   // Optional description for the button.
+    Locked: boolean;        // Disables the button entirely, removes functionality.
     Callback: () => void;   // Function to call when the button is pressed.
 }
